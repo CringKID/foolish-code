1b8e29ff3c7c9198565e0cda126b1228aca95f05
CringKID <819738341@qq.com>
2022-01-17 17:33:29 +0800

    std

--- cpp/luogu/P2xxx/P2656.cpp
+++ cpp/luogu/P2xxx/P2656.cpp
@@ -10,7 +10,7 @@ const int kMaxN = 1e6 + 5, kMod = 1e9 + 7, kInf = 1e9;
 float arr[kMaxN];
 int n, m, s, tot, head[kMaxN], to[kMaxN], nxt[kMaxN], edge[kMaxN], From[kMaxN], To[kMaxN], Val[kMaxN];
 void add (int from, int to_, int val) {
-  to[++tot] = to, edge[tot] = val;
+  to[++tot] = to_, edge[tot] = val;
   nxt[tot] = head[from], head[from] = tot;
 }
 int dfn[kMaxN], num, low[kMaxN], top, status[kMaxN], cnt;
