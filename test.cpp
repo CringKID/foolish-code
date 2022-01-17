dbc94a6ba13e4cc25a75f1cf701e0677f1402bff
CringKID <819738341@qq.com>
2022-01-17 17:45:27 +0800

    asdq

--- test.cpp
+++ test.cpp
@@ -7,11 +7,15 @@ using Pii = pair <int, int>;
 using Pll = pair <ll, ll>;
 const int kMaxN = 1e5 + 5, kMod = 1e9 + 7, kInf = 1e9;
 
+//#define contest
 int main () {
   ios :: sync_with_stdio (false);
   cin.tie (0), cout.tie (0);
+#ifdef contest
+  freopen (, , stdin);
+  freopen (, , stdout);
+#endif
 
-  //asdgasdqweidopvasldkjtoipwqevkalsdituqposjdgnja
-  // dasd/f/as/dg/as/dg/a/sdg/as/dg//asd/g/asd/g/asf/g/asd/g/a/sdg/
+  
   return 0;
 }
