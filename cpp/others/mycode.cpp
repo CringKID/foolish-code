5523bd15915376d10e600d11c1ba8f15291c76dd
CringKID <819738341@qq.com>
2022-01-17 21:44:15 +0800

    mycode

--- cpp/others/mycode.cpp
+++ cpp/others/mycode.cpp
@@ -1,68 +1,67 @@
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<cstring>
-#include<iostream>
-#include<cmath>
-#include<vector>
-#include<queue>
-#include<algorithm>
-#define inf 0x7fffffff
+#include <bits/stdc++.h>
 using namespace std;
-const int _N = 11000;
-const int _M = 31000;
-inline int read()
-{
-    char c = getchar(); int sign = 1; int x = 0;
-    while(c > '9' || c < '0') { if(c=='-')sign = -1; c = getchar(); } 
-    while(c <= '9' && c >= '0') { x *= 10; x += c - '0'; c = getchar(); }
-    return x * sign;
+namespace gengyf {
+#define ll long long
+const int maxn = 1e6 + 10;
+inline ll read() {
+  ll x = 0, f = 1;
+  char c = getchar();
+  while (c < '0' || c > '9') {
+    if (c == '-') f = -1;
+    c = getchar();
+  }
+  while (c >= '0' && c <= '9') {
+    x = (x * 10) + c - '0';
+    c = getchar();
+  }
+  return x * f;
 }
-int n, m;
-int head[_N + _M];
-struct edges{
-	int node;
-	int nxt;
-}edge[1100000];
-int tot = 0;
-void add(int u, int v)
-{
-	edge[++tot].nxt = head[u];
-	head[u] = tot;
-	edge[tot].node = v;
+ll n, k, m, a[maxn], t[maxn];
+int f[maxn][2], g[maxn];
+int main() {
+  n = read();
+  k = read();
+  m = read();
+  for (int i = 1; i <= n; i++) {
+    a[i] = read();
+  }
+  ll l = 1, r = k + 1, lim = log2(m) + 1;
+  for (int i = 1; i <= n; i++) {
+    while (r < n && a[r + 1] - a[i] < a[i] - a[l]) {
+      l++, r++;
+    }
+    f[i][0] = (a[r] - a[i] > a[i] - a[l] ? r : l);
+  }
+  if (m & 1) {
+    for (int i = 1; i <= n; i++) {
+      g[i] = f[i][0];
+    }
+  } else {
+    for (int i = 1; i <= n; i++) {
+      g[i] = i;
+    }
+  }
+  r = 1;
+  t[0] = 1;
+  for (int i = 1; i <= lim; i++) t[i] = t[i - 1] << 1;
+  for (int j = 1; j <= lim; j++) {
+    for (int i = 1; i <= n; i++) {
+      f[i][r] = f[f[i][r ^ 1]][r ^ 1];
+    }
+    if (m & t[j]) {
+      for (int i = 1; i <= n; i++) {
+        g[i] = f[g[i]][r];
+      }
+    }
+    r ^= 1;
+  }
+  for (int i = 1; i <= n; i++) {
+    printf("%d ", g[i]);
+  }
+  return 0;
 }
-bool vis[_N + _M];
-void dfs(int now)
-{
-	vis[now] = true; 
-	for(register int i = head[now];i;i = edge[i].nxt)
-	{
-		int exNode = edge[i].node;
-		if(vis[exNode]) continue;
-		dfs(exNode); 
-	}
-}
-int main()
-{
-	n = read(), m = read();
-	for(register int i = 1;i <= n;i++)
-	{
-		int k = read();
-		for(register int j = 1;j <= k;j++)
-		{
-			int tmp = read();
-			add(i, n + tmp);
-			add(n + tmp, i);
-		}
-	}
-	int ans = 0 ;
-	memset(vis, false, sizeof(vis));
-	for(register int i = 1;i <= n;i++)
-		if(!vis[i])
-		{
-			dfs(i);
-			ans ++;
-		}
-	printf("%d", ans - 1);
-    return 0;
+}  // namespace gengyf
+signed main() {
+  gengyf::main();
+  return 0;
 }
